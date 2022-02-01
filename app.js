3409563d64586974991eb1725efede999a66c7b4
Wing Luk <lukwing747@gmail.com>
2022-02-01 20:53:19 +0800

    v1

---
+++ app.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $("#ASM-button").click(function(){
+    $(".experience-description").html("<h4>ASM Technology Hong Kong Limited<br/> Electronic engineer<br/> 2019-2021</h4><p>Responsible for product design and development <br />● Design circuit (ADC convertor, DC/DC convertor etc.) <br />● Guide and review PCB layout designs<br />● Develop firmware for FPGA and MCU<br />● Create function test (FCT)/burn-in test program using C<br />● Create design, production, and testing documents<br />● Support users and manufacturing process<br />● Collaborate with test lab to conduct reliability test (EMC, HALT, etc)<br />● Troubleshoot using lab equipment <br /></p>"
+);
+  });
+
+
+$("#project-assistant-button").click(function(){
+  $(".experience-description").html("<div><h4>HKUST, Student Affairs Office<br/> Project assistant<br/> Fall 2017</h4><p>●	Data entry for health & fitness Assessments and course survey <br />●	Managed inventory and stock of sports equipment, team uniforms, and souvenir etc.<br />●	Conducted health & fitness assessments<br />  ●	Managed students’ registration, assessment schedule and attendance access<br /></p></div>"
+);
+});
+
+
+$("#robotics-button").click(function(){
+  $(".experience-description").html("<h4>HKUST Robotics Team<br/> Electronic engineer  <br/>2015- 2016</h4><p>Participated in The NXP Cup National University Students Intelligent Car Race 2016<br />● Built, programmed and raced a model car <br />● Drew PCB layout using Altium and solder PCB board <br />  ● Constructed mounting using Solidworks and 3D print<br />● Programmed on K60 32-bit microcontroller with C++ <br /></p>"
+);
+});
+
+
+$("#mtr-button").click(function(){
+  $(".experience-description").html("<h4>MTR <br/> Intern   <br/> Summer 2017 </h4><p>●	Assisted in factory acceptance tests<br /> ●	Performed electromagnetic compatibility tests<br />●	Visited construction site with senior engineer<br />●	Took minutes and followed up on projects<br />"
+);
+});
+
+
+
+});
